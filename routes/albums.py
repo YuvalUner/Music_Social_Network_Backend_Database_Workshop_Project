5b135f5948a18882939b21eb6171932e625495c0
ori-roza <ori@atidot.com>
2022-12-11 16:58:09 +0200

    init project BaseRepository executes queries each table has repository

--- routes/albums.py
+++ routes/albums.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, jsonify
-
 from repositories.albums import AlbumsRepository
 
 albums_routes = Blueprint('albums', __name__)
 
 
-@albums_routes.route('/')
+@albums_routes.route('/', methods=["GET"])
 def get_all_albums():
     albums = AlbumsRepository.get_instance().get_all_albums()
     return jsonify([{"album_id": album[0],
