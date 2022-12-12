ae96de282b31a2301e9793c05cdabb8059905a88
ori-roza <ori@atidot.com>
2022-12-12 11:31:35 +0200

    added get artist albums added top n rated artists

--- routes/artists.py
+++ routes/artists.py
@@ -5,8 +5,9 @@ artists_routes = Blueprint('artists', __name__)
 
 
 @artists_routes.route('/login', methods=["POST"])
-def check_artist_in_db(artist_name: str):
-    if not ArtistsRepository.get_instance().login_artist_check(artist_name):
+def check_artist_in_db():
+    if not ArtistsRepository.get_instance().login_artist_check(request.json["name"],
+                                                               request.json["password"]):
         return jsonify({"result": False}), 404
     return jsonify({"result": True}), 200
 
@@ -15,3 +16,15 @@ def check_artist_in_db(artist_name: str):
 def add_new_artist():
     ArtistsRepository.get_instance().add_artist(request.json["name"], request.json["password"])
     return jsonify({"res": "OK"}), 201
+
+
+@artists_routes.route('/<int:artist_id>/albums', methods=["GET"])
+def get_artist_albums(artist_id: int):
+    albums = ArtistsRepository.get_instance().get_artist_albums(artist_id)
+    return jsonify({"albums": albums}), 200
+
+
+@artists_routes.route('/top_rated/<int:n>', methods=["GET"])
+def get_top_rated_artists(n: int):
+    n_top_rated = ArtistsRepository.get_instance().get_highest_rated_artists(n)
+    return jsonify({"top_rated_artists": n_top_rated}), 200
