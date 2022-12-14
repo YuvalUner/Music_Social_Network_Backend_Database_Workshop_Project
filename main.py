52b6b0228f49991772812b14230229df19d9713c
Yuval Uner <yuvaluner@gmail.com>
2022-12-14 15:21:08 +0200

    Added songs_routes to routes __init__.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import routes
 from app_conf import app
-from routes import albums_routes
-from routes.songs import songs_routes
 
 if __name__ == '__main__':
-    app.register_blueprint(albums_routes, url_prefix='/albums')
-    app.register_blueprint(songs_routes, url_prefix='/songs')
+    app.register_blueprint(routes.albums_routes, url_prefix='/albums')
+    app.register_blueprint(routes.songs_routes, url_prefix='/songs')
 
     app.run(port=8080, debug=True)
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,2 @@
 from routes.albums import albums_routes
+from routes.songs import songs_routes
