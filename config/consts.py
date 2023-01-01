e8fc29d3799e7c843d25556776168cdbcb65aea9
amiram-yss <amiram-yss@users.noreply.github.com>
2023-01-01 12:32:08 +0200

    Added get albums query. For ratings, run get_album_by_name()

--- config/consts.py
+++ config/consts.py
@@ -5,5 +5,5 @@ load_dotenv("config/.env")
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_NAME = os.getenv("DB_NAME", "music_social_network")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "1234")
+DB_NAME = os.getenv("DB_NAME", "spotify") #RETURN to music_social_network later
